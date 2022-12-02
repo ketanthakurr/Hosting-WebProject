d3472de9f2d6225bad689652fd5b80c8622664c5
ketanthakurr <ketanthakur603@gmail.com>
2022-12-03 04:02:02 +0530

    Hosting-Web-Pages

---
+++ public/korea.js
@@ -0,0 +1,32 @@
+// navbar JS
+
+const navbar = document.querySelector('#navbar');
+
+window.onscroll = () => {
+    if (window.scrollY > 230) {
+        console.log("nzjbjbjbczjbczjbjbbj");
+        navbar.classList.add('navbar-scroll');
+    } else {
+        navbar.classList.remove('navbar-scroll');
+    }
+};;
+
+function openPage(){
+    var x = document.getElementById("search").value;
+
+    if(x === "greece"){
+        window.open("/greece")
+    }
+    if(x === "india"){
+        window.open("/india")
+    }
+    if(x === "korea"){
+        window.open("/korea")
+    }
+    if(x === "travel"){
+        window.open("/travel")
+    }
+    if(x === "budget"){
+        window.open("/budget")
+    }
+}
